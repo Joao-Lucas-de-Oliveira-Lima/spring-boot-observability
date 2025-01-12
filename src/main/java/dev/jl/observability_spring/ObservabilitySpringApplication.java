2232f3c7f3592cc1a11d5166ad08a3c5efda3675
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2025-01-11 21:42:34 -0300

    initial commit

---
+++ src/main/java/dev/jl/observability_spring/ObservabilitySpringApplication.java
@@ -0,0 +1,13 @@
+package dev.jl.observability_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilitySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservabilitySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jl/observability_spring/ObservabilitySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jl.observability_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilitySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
